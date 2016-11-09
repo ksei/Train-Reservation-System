909afd9c7104d4f1730b5f3ffbae33cb6171b414
Ksandros <ksandrosapostoli@hotmail.com>
2016-11-09 16:39:43 +0200

    Confirmation model

---
+++ TrainReservation/Models/BookingConfirmationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainReservation.Models
+{
+    public class BookingConfirmationModel
+    {
+        
+            public Trip Trips { get; set; }
+            public Bookings Bookings { get; set; }
+      
+    }
+}
